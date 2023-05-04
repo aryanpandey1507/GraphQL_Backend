b278be6220763ad01b80ab8f623159026938d07b
Aryan Pandey <aryanpandey1507@gmail.com>
2023-05-04 12:31:09 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async ()=>{
     const conn = await mongoose.connect(process.env.DATABASE_URL);
-    console.log(`Mongo connected at ${conn.connection.host}`.cyan.underline.bold);
+//     console.log(`Mongo connected at ${conn.connection.host}`.cyan.underline.bold);
+    console.log("DB connected")
 };
 
 module.exports = connectDB;
